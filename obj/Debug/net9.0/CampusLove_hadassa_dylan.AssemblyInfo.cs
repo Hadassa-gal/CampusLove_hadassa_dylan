df534e157333416f98c041a476bef3b186deeccb
Dylan3-cpu <194501634+Dylan3-cpu@users.noreply.github.com>
2025-08-25 12:39:06 -0500

    Merge

--- obj/Debug/net9.0/CampusLove_hadassa_dylan.AssemblyInfo.cs
+++ obj/Debug/net9.0/CampusLove_hadassa_dylan.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampusLove_hadassa_dylan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bb03f0375d9463f9dc6fd3b8de4a52bbfaa0343")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4646f2e783dfea9672e078fc3739f03a57ed6f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampusLove_hadassa_dylan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampusLove_hadassa_dylan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
